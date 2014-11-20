7d063a6cdf433611b44ca1741425d5cadcb46674
ROUABHI <git@wikeo.mobi>
2014-11-20 14:19:09 +0100

    Avoid logging

--- index.js
+++ index.js
@@ -18,10 +18,10 @@ function getDB(db) {
 		dbhost = databases[db][3],
 		dbport = databases[db][4];
 	if (!dbuser) databases[db] = new (Sequelize)(dbname, {language:'en',
-						 //logging:console.log, 
+						 logging:new Function(), 
 						 define:{freezeTableName: true}});
 	else databases[db] = new (Sequelize)(dbname,dbuser,dbpass, {host:dbhost, port:dbport, language:'en',
-						 //logging:console.log, 
+						 logging:new Function(), 
 						 define:{freezeTableName: true}});
 	return databases[db];
 }
